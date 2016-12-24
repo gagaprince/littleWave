48c2e66ee53beb3a68e510e01f5ee8c0030d6ba2
zidong.wang <wangzidongprince@163.com>
2016-12-25 02:29:40 +0800

    cha bu duo

--- confige/conf.js
+++ confige/conf.js
@@ -1,4 +1,4 @@
 module.exports={
-    publishPath : 'D:/work/fepublish/littleWave',   //发布地址
-    devWebpackPath : 'D:/work/myselfwork/blog/src/main/webapp/blog/fetexiao/littleWave' //dev测试地址
+    publishPath : '/Users/gagaprince/work/server/blog/src/main/webapp/blog/fetexiao/littleWave',   //发布地址
+    devWebpackPath : '/Users/gagaprince/work/server/blog/src/main/webapp/blog/fetexiao/littleWave' //dev测试地址
 }
